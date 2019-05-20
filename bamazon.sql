8daceddf2b08875214088d4bb31f04961ba2fffd
Waqar Habib <waqarhabib94@aggienetwork.com>
2019-05-20 14:17:18 -0500

    updated store with items

--- bamazon.sql
+++ bamazon.sql
@@ -4,21 +4,21 @@ USE bamazon_DB;
 DROP TABLE products;
 
 CREATE TABLE IF NOT EXISTS products (
-	id INT(10) AUTO_INCREMENT,
+	id INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     product_name VARCHAR(50) NOT NULL,
     department_name VARCHAR(50) NOT NULL,
     price DECIMAL(6,2) NOT NULL,
-    stock_quantity INT DEFAULT 0,
-    PRIMARY KEY (id)
+    stock_quantity INT DEFAULT 0
+    
 );
 
-INSERT INTO products VALUES ('1', 'Apple iPhone X 64 GB', 'Electronics', '849.99', '2');
-INSERT INTO products VALUES ('2', 'Becoming', 'Books', '16.63', '9');
-INSERT INTO products VALUES ('3', 'The Moment of Lift', 'Books', '15.89', '31');
-INSERT INTO products VALUES ('4', 'Apple 15-inch MacBook Pro', 'Electronics', '2799.99', '1');
-INSERT INTO products VALUES ('5', 'Monopoly Empire Game', 'Games', '32.22', '4');
-INSERT INTO products VALUES ('6', 'TaoTronics LED Desk Lamp', 'Electronics', '29.99', '6');
-INSERT INTO products VALUES ('7', 'CocoMagic Hand Cream', 'Beauty', '9.99', '7');
-INSERT INTO products VALUES ('8', 'Essential Oil 3-pack', 'Beauty', '16.99', '10');
-INSERT INTO products VALUES ('9', 'Melatonin 10 mg', 'Health', '9.79', '0');
-INSERT INTO products VALUES ('10', 'Optimum Nutrition Whey 5 LB', 'Health', '54.99', '50');
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('Cape Cod Original Kettle Chips', 'Snacks', 2.88, 25);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('Bamazing Whole Milk, 1 Gallon', 'Dairy', 2.28, 35);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('Bamazing Bake Shop 100% Whole Wheat Bread', 'Bakery', 1.07, 12);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('New York Garlic Texas Toast, 8 ct.', 'Frozen Food', 2.22, 2);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('Coca-Cola Classic Coke 12 oz Cans', 'Drinks', 5.27, 20);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('Bamazing Guac, 7 oz', 'Snacks', 4.98, 3);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('Galbani Presliced Mozzarella Fresca Log, 16 oz', 'Dairy', 4.97, 13);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('Babybel Mini Lights, 7.5oz', 'Dairy', 4.55, 21);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('Fresh Organic Strawberries, 1 lb', 'Produce', 2.56, 40);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('Tyson Fully Cooked Chicken Nuggets, 32 oz', 'Frozen Food', 4.48, 41);
