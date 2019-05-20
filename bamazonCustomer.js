b36f9b19fda1a2173cbeafef62d280c4be57c387
Waqar Habib <waqarhabib94@aggienetwork.com>
2019-05-20 13:59:30 -0500

    refactored the entire code due to errors, initial function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,55 +12,28 @@ const connection = mysql.createConnection({
 connection.connect(function(err) {
     if(err) throw err;
     //console.log(`MySQL connected on ${connection.threadId}`);
-
-    displayInventory();
 });
 
-function displayInventory() {
-    connection.query("SELECT id, product_name, department_name, price, stock_quantity FROM products", function(err, res) {
-        if (err) throw err;
-        console.log("Welcome to Bamazon Store! Here's everything that we have in stock...");
-        res.forEach(element => {
-            var id = element.id;
-            var product_name = element.product_name;
-            var department_name = element.department_name;
-            var price = element.price;
-            var stock_quantity = element.stock_quantity;
-          console.log(`ID: #${id} - Product: ${product_name} from ${department_name} for $${price} ~~ Available: ${stock_quantity}`);
-          });
-    })
-}
-/*
-function purchaseProducts() {
-    inquirer
-        .prompt({
-            name: "id",
-            type: "input",
-            message: "What would you like to buy today? Please type in the ID number to continue with your purchase.",
-            validate: function(value){
-                if(isNaN(value)===false){
-                    return true;
-                } else {
-                    return false;
+purchaseItem();
+
+function purchaseItem() {
+    connection.query('SELECT * FROM products', function(err, res){
+        // display products and price to user
+        for (var i = 0; i < res.length; i++) {
+            console.log('Item: ' + res[i].product_name + ' | Price: ' + res[i].price + ' | Available: ' + res[i].stock_quantity);
+        }
+        
+        // Inquirer prompts 
+        inquirer.prompt([{
+            // Choose Product
+            name: "choice",
+            type: "rawlist",
+            message: "What would you like to purchase today from Bamazon Pantry?",
+            choices: function(value) {
+                var itemsArray = [];
+                for (var i = 0; i < res.length; i++) {
+                    itemsArray.push(res[i].product_name);
                 }
+                return itemsArray;
             }
-        },
-        {
-            name: "units",
-            type: "input",
-            message: "How many would you like to purchase?",
-            validate: function(value){
-                if(isNaN(value)===false){
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-        })
-        .then(function(input){
-            id = input.id;
-            units = Number(input.units);
-        //checkInventory();
-        });
-}
-*/
+        }, 
